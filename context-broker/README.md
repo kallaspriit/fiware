83a1223098015c1aca65c9e60b4c3ce5a2a12605
Priit Kallas <kallaspriit@gmail.com>
2016-02-18 16:30:58 +0200

    Implemented updating entity attribute.

--- context-broker/README.md
+++ context-broker/README.md
@@ -2,7 +2,17 @@ Orion context broker
 ====================
 
 Running the example
------------
+-------------------
 Execute in command line
 - `npm install` to install dependencies
 - `npm run start` to execute the script
+
+Password configuration file
+---------------------------
+Do not have to always enter username and password when testing it, create a `credentials.json` file with contents like
+```json
+{
+  "username": "me@example.com",
+  "password": "secret"
+}
+```
--- context-broker/index.js
+++ context-broker/index.js
@@ -14,17 +14,42 @@ const config = {
 	}
 };
 
+const Method = {
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT'
+};
+
+function loadCredentialsFromFile() {
+	try {
+		const info = JSON.parse(fs.readFileSync('credentials.json', 'UTF8'));
+
+		return {
+			username: info.username || null,
+			password: info.password || null
+		}
+	} catch (e) {
+		return {
+			username: null,
+			password: null
+		};
+	}
+}
 
 function promptCredentials() {
 	return new Promise((resolve) => {
+		const fileCredentials = loadCredentialsFromFile();
+
 		inquirer.prompt([{
 			type: 'input',
 			name: 'username',
-			message: 'Enter FIWARE username:'
+			message: 'Enter FIWARE username:',
+			default: fileCredentials.username
 		}, {
 			type: 'password',
 			name: 'password',
 			message: 'Enter FIWARE password:',
+			default: fileCredentials.password
 		}], (answers) => {
 		    const { username, password } = answers;
 
@@ -36,11 +61,11 @@ function promptCredentials() {
 	});
 }
 
-function queryApi(path, data) {
+function queryApi(method, path, data) {
 	return new Promise((resolve, reject) => {
 		request({
 			url: config.api.url + '/' + path,
-			method: 'POST',
+			method: method,
 			headers: {
 				'Content-type': 'application/json'
 			},
@@ -59,14 +84,14 @@ function queryApi(path, data) {
 	});
 }
 
-function queryBroker(token, path, data = {}) {
+function queryBroker(method, token, path, data = {}) {
 	return new Promise((resolve) => {
 		const baseUrl = config.api.broker.url + ':' + config.api.broker.port + '/' + config.api.broker.version;
 		const url = baseUrl + '/' + path;
 
 		request({
 			url: url,
-			method: 'GET',
+			method: method,
 			headers: {
 				'Content-type': 'application/json'
 			},
@@ -90,18 +115,42 @@ function queryBroker(token, path, data = {}) {
 	});
 }
 
+function fetchToken({ username, password }) {
+	return queryApi(
+		Method.POST,
+		'token', {
+			username: username,
+			password: password
+		}
+	);
+}
+
 function fetchEntity(name) {
 	return queryBroker(
+		Method.GET,
 		config.api.token,
 		'contextEntities/' + name
 	);
 }
 
-function fetchToken({ username, password }) {
-	return queryApi(
-		'token', {
-			username: username,
-			password: password
+function createEntity(id, attributes) {
+	return queryBroker(
+		Method.POST,
+		config.api.token,
+		'contextEntities/' + id,
+		{
+			attributes: attributes
+		}
+	);
+}
+
+function updateEntityAttribute(id, attribute, value) {
+	return queryBroker(
+		Method.PUT,
+		config.api.token,
+		'contextEntities/' + id + '/attributes/' + attribute,
+		{
+			value: value
 		}
 	);
 }
@@ -114,11 +163,17 @@ function handleToken(token) {
 
 function handleResponse(name) {
 	return (response) => {
-		const responseText = JSON.stringify(response, null, '  ');
+		const responseText = JSON.stringify(response, null, '  ') + '\n';
 
 		console.log(name, responseText);
 	};
 }
+
+function generateUniqueId() {
+	return Math.floor(Math.random() * 1000000000);
+}
+
+const uniqueEntityId = 'TestEntity-' + generateUniqueId();
 
 // ask for user username/password
 promptCredentials()
@@ -134,6 +189,30 @@ promptCredentials()
 	// fetch and display traffic sensor info
 	.then(() => fetchEntity('urn:smartsantander:testbed:3332'))
 	.then(handleResponse('traffic sensor'))
+
+	// create a new entity
+	.then(() => createEntity(uniqueEntityId, [{
+            "name": "city_location",
+            "type": "city",
+            "value": "Tartu"
+        }, {
+            "name": "temperature",
+            "type": "float",
+            "value": "3.2"
+    }]))
+	.then(handleResponse('created entity "' + uniqueEntityId + '"'))
+
+	// fetch created sensor info
+	.then(() => fetchEntity(uniqueEntityId))
+	.then(handleResponse('created sensor info'))
+
+	// update created entity value
+	.then(() => updateEntityAttribute(uniqueEntityId, 'temperature', '-6.2'))
+	.then(handleResponse('updated entity "' + uniqueEntityId + '"'))
+
+	// fetch updated sensor info
+	.then(() => fetchEntity(uniqueEntityId))
+	.then(handleResponse('updated sensor info'))
 
 	// handle any errors
 	.catch((error) => {
