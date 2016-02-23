d62116112334cf92a53e8178f8711342d7fb6799
Priit Kallas <kallaspriit@gmail.com>
2016-02-23 13:22:58 +0200

    Configuration is now in a separate file.

---
+++ context-broker/config.js
@@ -0,0 +1,4 @@
+export default {
+	apiUrl: 'https://orion.lab.fiware.org',
+	brokerUrl: 'http://orion.lab.fiware.org:1026'
+};
--- context-broker/lab.js
+++ context-broker/lab.js
@@ -1,12 +1,7 @@
+import config from './config';
 import util from './lib/util';
 import Api from './lib/fiware/Api';
 import ContextBroker from './lib/fiware/ContextBroker';
-
-// configuration
-const config = {
-	apiUrl: 'https://orion.lab.fiware.org',
-	brokerUrl: 'http://orion.lab.fiware.org:1026'
-};
 
 // setup APIs
 const api = new Api({
@@ -15,6 +10,54 @@ const api = new Api({
 const contextBroker = new ContextBroker({
 	url: config.brokerUrl
 });
+
+// generate unique entity id
+const uniqueEntityId = 'TestEntity-' + generateUniqueId();
+
+// ask for user username/password
+promptCredentials()
+
+	// fetch, remember and display the access token
+	.then(({ username, password }) => api.fetchToken(username, password))
+	.then(handleTokenResponse)
+
+	// fetch and display traffic sensor info
+	.then(() => contextBroker.fetchEntity('urn:smartsantander:testbed:357'))
+	.then(handleQueryResponse('sound level meter'))
+
+	// fetch and display traffic sensor info
+	.then(() => contextBroker.fetchEntity('urn:smartsantander:testbed:3332'))
+	.then(handleQueryResponse('traffic sensor'))
+
+	// create a new entity
+	.then(() => contextBroker.createEntity(uniqueEntityId, [{
+		name: 'city_location',
+		type: 'city',
+		value: 'Tartu'
+	}, {
+		name: 'temperature',
+		type: 'float',
+		value: 3.2
+	}]))
+	.then(handleQueryResponse('created entity "' + uniqueEntityId + '"'))
+
+	// fetch created sensor info
+	.then(() => contextBroker.fetchEntity(uniqueEntityId))
+	.then(handleQueryResponse('created sensor info'))
+
+	// update created entity value
+	.then(() => contextBroker.updateEntityAttribute(uniqueEntityId, 'temperature', -6.2))
+	.then(handleQueryResponse('updated entity "' + uniqueEntityId + '"'))
+
+	// fetch updated sensor info
+	.then(() => contextBroker.fetchEntity(uniqueEntityId))
+	.then(handleQueryResponse('updated sensor info'))
+
+	// handle any errors
+	.catch((error) => {
+		console.error(error.stack);
+	});
+
 
 // prompts for user credentials
 function promptCredentials() {
@@ -65,50 +108,3 @@ function handleQueryResponse(name) {
 		console.log(name, responseText);
 	};
 }
-
-// generate unique entity id
-const uniqueEntityId = 'TestEntity-' + generateUniqueId();
-
-// ask for user username/password
-promptCredentials()
-
-	// fetch, remember and display the access token
-	.then(({ username, password }) => api.fetchToken(username, password))
-	.then(handleTokenResponse)
-
-	// fetch and display traffic sensor info
-	.then(() => contextBroker.fetchEntity('urn:smartsantander:testbed:357'))
-	.then(handleQueryResponse('sound level meter'))
-
-	// fetch and display traffic sensor info
-	.then(() => contextBroker.fetchEntity('urn:smartsantander:testbed:3332'))
-	.then(handleQueryResponse('traffic sensor'))
-
-	// create a new entity
-	.then(() => contextBroker.createEntity(uniqueEntityId, [{
-		name: 'city_location',
-		type: 'city',
-		value: 'Tartu'
-	}, {
-		name: 'temperature',
-		type: 'float',
-		value: 3.2
-	}]))
-	.then(handleQueryResponse('created entity "' + uniqueEntityId + '"'))
-
-	// fetch created sensor info
-	.then(() => contextBroker.fetchEntity(uniqueEntityId))
-	.then(handleQueryResponse('created sensor info'))
-
-	// update created entity value
-	.then(() => contextBroker.updateEntityAttribute(uniqueEntityId, 'temperature', -6.2))
-	.then(handleQueryResponse('updated entity "' + uniqueEntityId + '"'))
-
-	// fetch updated sensor info
-	.then(() => contextBroker.fetchEntity(uniqueEntityId))
-	.then(handleQueryResponse('updated sensor info'))
-
-	// handle any errors
-	.catch((error) => {
-		console.error(error.stack);
-	});
