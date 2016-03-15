43a1b55a6b0429a80f651ee30608cf2ffbebe6be
Priit Kallas <kallaspriit@gmail.com>
2016-03-15 10:16:47 +0200

    Improved intensity chart.

--- context-broker/apps/chart/chart.js
+++ context-broker/apps/chart/chart.js
@@ -16,7 +16,8 @@ var config = {
 
 var previousItemCount = 0;
 
-$('#container').highcharts({
+// draw the chart
+$('#chart-container').highcharts({
 	chart: {
 		type: 'spline',
 		zoomType: 'x',
@@ -37,7 +38,9 @@ $('#container').highcharts({
 						var newItems = data.slice(data.length - newItemCount);
 						var currentValue = Number.parseFloat(attributes[config.attributeName].value);
 
-						$('#current-value').html(currentValue + '%');
+						var valueChart = Highcharts.charts[$("#value-container").data('highchartsChart')];
+
+						valueChart.series[0].points[0].update(currentValue);
 
 						if (previousItemCount === 0) {
 							series.setData(data.slice(1));
@@ -114,6 +117,108 @@ $('#container').highcharts({
 		data: []
 	}],
 	legend: {
+		enabled: false
+	},
+	credits: {
+		enabled: false
+	}
+});
+
+$('#value-container').highcharts({
+
+	chart: {
+		type: 'gauge',
+		plotBackgroundColor: null,
+		plotBackgroundImage: null,
+		plotBorderWidth: 0,
+		plotShadow: false,
+		backgroundColor: 'rgba(255, 255, 255, 0)'
+	},
+
+	title: {
+		text: null
+	},
+
+	pane: {
+		startAngle: -150,
+		endAngle: 150,
+		background: [{
+			backgroundColor: {
+				linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+				stops: [
+					[0, '#FFF'],
+					[1, '#333']
+				]
+			},
+			borderWidth: 0,
+			outerRadius: '109%'
+		}, {
+			backgroundColor: {
+				linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+				stops: [
+					[0, '#333'],
+					[1, '#FFF']
+				]
+			},
+			borderWidth: 1,
+			outerRadius: '107%'
+		}, {
+			// default background
+		}, {
+			backgroundColor: '#DDD',
+			borderWidth: 0,
+			outerRadius: '105%',
+			innerRadius: '103%'
+		}]
+	},
+
+	// the value axis
+	yAxis: {
+		min: 0,
+		max: 100,
+
+		minorTickInterval: 'auto',
+		minorTickWidth: 1,
+		minorTickLength: 10,
+		minorTickPosition: 'inside',
+		minorTickColor: '#666',
+
+		tickPixelInterval: 30,
+		tickWidth: 2,
+		tickPosition: 'inside',
+		tickLength: 10,
+		tickColor: '#666',
+		labels: {
+			step: 2,
+			rotation: 'auto'
+		},
+		title: {
+			text: 'intensity'
+		},
+		plotBands: [{
+			from: 0,
+			to: 70,
+			color: '#333' // green
+		}, {
+			from: 70,
+			to: 85,
+			color: '#CCC' // yellow
+		}, {
+			from: 85,
+			to: 100,
+			color: '#FFF' // red
+		}]
+	},
+
+	series: [{
+		name: 'Light intensity',
+		data: [0],
+		tooltip: {
+			valueSuffix: ' %'
+		}
+	}],
+
+	credits: {
 		enabled: false
 	}
 });
